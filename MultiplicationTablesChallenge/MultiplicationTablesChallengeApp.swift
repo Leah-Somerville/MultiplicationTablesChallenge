82299deb1891421652b9517697b863d8d7462fc9
Leah Somerville <leah.somerville4@gmail.com>
2024-03-07 09:44:05 -0600

    Initial Commit

---
+++ MultiplicationTablesChallenge/MultiplicationTablesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesChallengeApp.swift
+//  MultiplicationTablesChallenge
+//
+//  Created by Leah Somerville on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
